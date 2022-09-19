755e1d797b15df83f79b9f90288c4d66454fdd5c
Michal <example@gmail.com>
2022-09-19 12:32:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+//TYPEOFF
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt(" Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет??', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// ИНТЕРПОЛЯЦИЯ
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Michal';
+
+// alert(`Привет, ${user}`)
+
+
+// ОПЕРАТОРЫ
+console.log('arr' + "- object");
+console.log('4' + "- object");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+   
+   console.log(incr++);
+   console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+
+const isChecked = true,
+       isClose = true;
+       
+console.log(isChecked && isClose);
+
+console.log(isChecked || isClose);
+
